8f7e071761be191384d29cbe874ab6dbc55faa41
Joel Shepherd <joel@swivid.ink>
2019-09-09 18:31:07 +1000

    Type experiment

--- src/mod.ts
+++ src/mod.ts
@@ -1,28 +1,32 @@
-export const any = Symbol("any")
-export const boolean = Symbol("boolean")
-export const number = Symbol("number")
-export const string = Symbol("string")
+type Constructor = (...args: any[]) => any
+type Value = string | number
+type On = string | number | Constructor
+type Matched<T> = T extends Constructor ? ReturnType<T> : T
+type Action<T> = (value: Matched<T>) => any
+type Compare = (value: Value) => boolean
+type When<O = any> = [Compare, Action<O>]
 
-const typeMap = {
-  boolean,
-  number,
-  string,
-  bigint: undefined,
-  object: undefined,
-  function: undefined,
-  symbol: undefined,
-  undefined: undefined,
+export function match(value: Value, whens: When[]) {
+  for (const [compare, action] of whens) {
+    if (compare(value)) return action(value)
+  }
 }
 
-type TypeSymbol = typeof any | typeof number | typeof string
-
-type Index = number | string | TypeSymbol
-
-export function match<
-  R,
-  V extends number | string,
-  C extends Partial<Record<Index, (value: V) => any>>
->(value: V, when: C): R | undefined {
-  const fn = when[value] || when[typeMap[typeof value] as any] || when[any]
-  if (fn) return fn(value)
+export function when<O extends On>(on: O, action: Action<O>): When<O> {
+  return [value => on === value, action]
 }
+
+// Test examples
+
+const result = match("value", [
+  when(42, x => "The answer to life, the universe, and everything."),
+  when(String, x => `Hello {x}!`),
+  when(Number, x => `You got: ${x}`),
+  when(Error, x => `Oh no! ${x.message}`),
+])
+
+match("best", [
+  when("good", () => "no"),
+  when("better", () => "no"),
+  when("best", () => "yes"),
+])
