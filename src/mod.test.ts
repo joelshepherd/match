26bfc7c8164a56bb67febae813d34678506c384e
Joel Shepherd <joel@swivid.ink>
2020-11-23 16:57:55 +1000

    Updating deps

--- src/mod.test.ts
+++ src/mod.test.ts
@@ -1,149 +1,158 @@
-import { expect } from "chai"
-import { match, otherwise, when } from "./mod"
+import { expect } from "chai";
+import { match, otherwise, when } from "./mod";
 
-describe("match()", function() {
-  it("should match exact values", function() {
+describe("match()", function () {
+  it("should match exact values", function () {
     expect(
       match(2, [
         when(1, () => "incorrect"),
         when(2, () => "correct"),
         when(3, () => "incorrect"),
-      ]),
-    ).to.equal("correct")
+      ])
+    ).to.equal("correct");
 
     expect(
       match("best", [
         when("good", () => "no"),
         when("better", () => "nah"),
         when("best", () => "yes"),
-      ]),
-    ).to.equal("yes")
+      ])
+    ).to.equal("yes");
 
     expect(
-      match(false, [when(true, () => "no"), when(false, () => "si")]),
-    ).to.equal("si")
-  })
+      match(false, [when(true, () => "no"), when(false, () => "si")])
+    ).to.equal("si");
+  });
 
-  it("should match regular expressions", function() {
+  it("should match regular expressions", function () {
     expect(
-      match("text", [when(/not/, () => "no"), when(/text/, () => "yes")]),
-    ).to.equal("yes")
-  })
+      match("text", [when(/not/, () => "no"), when(/text/, () => "yes")])
+    ).to.equal("yes");
+  });
 
-  it("should match constructor types", function() {
+  it("should match constructor types", function () {
     expect(
       match("text", [
         when(1, () => "incorrect"),
         when("not", () => "incorrect"),
         when(String, () => "correct"),
-      ]),
-    ).to.equal("correct")
+      ])
+    ).to.equal("correct");
 
     expect(
       match(99, [
         when(10, () => "no"),
         when(Number, () => "yes"),
         otherwise(() => "no"),
-      ]),
-    ).to.equal("yes")
-  })
+      ])
+    ).to.equal("yes");
+  });
 
-  it("should match custom match functions", function() {
+  it("should match custom match functions", function () {
     expect(
       match(10, [
-        when((x: number) => x === 1, () => "no"),
-        when((x: number) => x === 10, () => "yes"),
-        when((x: number) => x === 100, () => "no"),
-      ]),
-    ).to.equal("yes")
-  })
+        when(
+          (x: number) => x === 1,
+          () => "no"
+        ),
+        when(
+          (x: number) => x === 10,
+          () => "yes"
+        ),
+        when(
+          (x: number) => x === 100,
+          () => "no"
+        ),
+      ])
+    ).to.equal("yes");
+  });
 
-  it("should shallow match objects", function() {
+  it("should shallow match objects", function () {
     expect(
       match({ foo: "bar" }, [
         when(1, () => "wrong"),
         when(String, () => "nope"),
         when({ foo: "bar" }, () => "right"),
-      ]),
-    ).to.equal("right")
+      ])
+    ).to.equal("right");
 
     expect(
       match({ status: 200 }, [
         when({ status: 404 }, () => "not correct"),
         when({ status: 500 }, () => "not correct"),
         when({ status: 200 }, () => "correct"),
-      ]),
-    ).to.equal("correct")
-  })
+      ])
+    ).to.equal("correct");
+  });
 
-  it("should match object keys as conditions", function() {
+  it("should match object keys as conditions", function () {
     expect(
       match({ method: "GET", path: "/test" }, [
         when({ status: "GET", path: /tset/ }, () => "failure"),
         when({ method: "GET", path: /test/ }, () => "success"),
-      ]),
-    ).to.equal("success")
+      ])
+    ).to.equal("success");
 
     expect(
       match({ status: 200, headers: { "Content-Type": "application/json" } }, [
         when(
           { status: 200, headers: { "Content-Type": "application/text" } },
-          () => "false",
+          () => "false"
         ),
         when(
           { status: 200, headers: { "Content-Type": "application/json" } },
-          () => "true",
+          () => "true"
         ),
-      ]),
-    ).to.equal("true")
+      ])
+    ).to.equal("true");
 
     expect(
       match({ status: 503 }, [
         when({ status: /4\d{2}/ }, () => "client error"),
         when({ status: /2\d{2}/ }, () => "success status"),
         when({ status: /5\d{2}/ }, () => "server error"),
-      ]),
-    ).to.equal("server error")
-  })
+      ])
+    ).to.equal("server error");
+  });
 
-  it("should match otherwise fallbacks", function() {
+  it("should match otherwise fallbacks", function () {
     expect(
       match("not going to", [
         when(1, () => "incorrect"),
         when("nope", () => "incorrect"),
         when(Number, () => "incorrect"),
         otherwise(() => "correct"),
-      ]),
-    ).to.equal("correct")
-  })
+      ])
+    ).to.equal("correct");
+  });
 
-  it("should return undefined if no matches", function() {
+  it("should return undefined if no matches", function () {
     expect(
       match(11, [
         when(10, () => "no"),
         when("text", () => "no"),
         when(String, () => "no"),
-      ]),
-    ).to.be.undefined
-  })
+      ])
+    ).to.be.undefined;
+  });
 
-  it("should pass value into match functions", function() {
-    expect(match(50, [when(Number, x => x * 2)])).to.equal(100)
+  it("should pass value into match functions", function () {
+    expect(match(50, [when(Number, (x) => x * 2)])).to.equal(100);
 
-    expect(match("hello", [when(String, x => `Text was ${x}`)])).to.equal(
-      "Text was hello",
-    )
+    expect(match("hello", [when(String, (x) => `Text was ${x}`)])).to.equal(
+      "Text was hello"
+    );
 
     expect(
       match({ status: 200, body: "Hello world" }, [
-        when({ status: 200 }, x => x.body),
-      ]),
-    ).to.equal("Hello world")
-  })
+        when({ status: 200 }, (x) => x.body),
+      ])
+    ).to.equal("Hello world");
+  });
 
-  it("should pass through promises", async function() {
+  it("should pass through promises", async function () {
     expect(
-      await match(true, [otherwise(() => Promise.resolve("promised"))]),
-    ).to.equal("promised")
-  })
-})
+      await match(true, [otherwise(() => Promise.resolve("promised"))])
+    ).to.equal("promised");
+  });
+});
--- src/mod.ts
+++ src/mod.ts
@@ -1,65 +1,66 @@
 /** Match the value against the conditions and return the result. */
 export function match(expression: Expression, whens: When[]) {
   for (const [compare, then] of whens) {
-    if (compare(expression)) return then(expression)
+    if (compare(expression)) return then(expression);
   }
 }
 
 /** Default case which matches any value. */
 export function otherwise(then: Then<any, any>): When {
-  return [() => true, then]
+  return [() => true, then];
 }
 
 /** Add a new match case. */
 export function when<P extends Condition, R>(
   condition: P,
-  then: Then<P, R>,
+  then: Then<P, R>
 ): When<P, R> {
-  return [createCompare(condition), then]
+  return [createCompare(condition), then];
 }
 
 function createCompare(condition: Condition): Compare {
   // Match regex
   if (condition instanceof RegExp) {
-    return value => condition.test(String(value))
+    return (value) => condition.test(String(value));
   }
   // Match constructor
   if (isConstructor(condition)) {
-    return value => condition === value.constructor
+    return (value) => condition === value.constructor;
   }
   if (typeof condition === "function") {
-    return value => condition(value)
+    return (value) => condition(value);
   }
   // Match object keys
   if (typeof condition === "object") {
-    return value => typeof value === "object" && compareObject(condition, value)
+    return (value) =>
+      typeof value === "object" && compareObject(condition, value);
   }
   // Exact match
-  return value => Object.is(condition, value)
+  return (value) => Object.is(condition, value);
 }
 
 function isConstructor(value: any): value is Constructor {
-  return Boolean(value && value.prototype && value.prototype.constructor.name)
+  return Boolean(value && value.prototype && value.prototype.constructor.name);
 }
 
 function compareObject(condition: object, value: object) {
-  if (Object.is(condition, value)) return true
+  if (Object.is(condition, value)) return true;
 
-  const keys = Object.getOwnPropertyNames(condition)
+  const keys = Object.getOwnPropertyNames(condition);
 
   for (const key of keys) {
     // @ts-ignore
     if (!createCompare(condition[key])(value[key])) {
-      return false
+      return false;
     }
   }
 
-  return true
+  return true;
 }
 
-type BuiltIn = (...args: any[]) => any
-type Constructor = new (...args: any[]) => any
-type Expression = boolean | number | object | string
+type BuiltIn = (...args: any[]) => any;
+type Constructor = new (...args: any[]) => any;
+type Expression = boolean | number | object | string;
 type Condition =
   | boolean
   | number
@@ -68,7 +69,7 @@ type Condition =
   | Compare
   | RegExp
   | BuiltIn
-  | Constructor
+  | Constructor;
 type ActionType<T extends Condition> = T extends RegExp
   ? ReturnType<T["exec"]>
   : T extends BuiltIn
@@ -77,7 +78,7 @@ type ActionType<T extends Condition> = T extends RegExp
   ? InstanceType<T>
   : T extends object
   ? T & Record<keyof any, unknown>
-  : T
-type Then<T extends Condition, R> = (value: ActionType<T>) => R
-type Compare = (value: Expression) => boolean
-export type When<O extends Condition = any, R = any> = [Compare, Then<O, R>]
+  : T;
+type Then<T extends Condition, R> = (value: ActionType<T>) => R;
+type Compare = (value: Expression) => boolean;
+export type When<O extends Condition = any, R = any> = [Compare, Then<O, R>];
